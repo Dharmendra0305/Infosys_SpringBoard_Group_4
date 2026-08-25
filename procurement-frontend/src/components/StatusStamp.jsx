74a414c80f4fd8d63d1c349545cf2f631f87232f
santhosh111305 <santhoshprabhu1311@gmail.com>
2026-08-25 10:39:14 +0530

    Delete procurement frontend

--- procurement-frontend/src/components/StatusStamp.jsx
+++
@@ -1,5 +0,0 @@
-export default function StatusStamp({ status }) {
-  if (!status) return null;
-  const cls = status.toLowerCase();
-  return <span className={`stamp ${cls}`}>{status.replace(/_/g, ' ')}</span>;
-}
